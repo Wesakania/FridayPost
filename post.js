0448a7f5074fdd13bc4d1c9bec2d25101dd50677
Wesakania <wesakaniadavis@gmail.com>
2022-12-03 00:39:04 +0300

    newPost

---
+++ post.js
@@ -0,0 +1,8 @@
+let jina = "Davis ";
+     jina = "Wesakania";
+name3 = jina ;
+function salamu(){
+  let greetings = prompt("How are you girl?" +",");
+  console.log(greetings +" "+ name3+ "?");
+}
+salamu();
